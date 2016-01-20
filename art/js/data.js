fa9d5ca5b50806fb88a4c0372e342b0329a31bb7
Jabez Wesly <wjabezkingsly@gmail.com>
2016-01-19 20:17:40 -0500

    Museum framework mostly finalized

--- art/js/data.js
+++ art/js/data.js
@@ -1,18 +1,26 @@
-var data = [{
-	"title":"Delacroix, Eugene",
-	"description":"Delacroix was the greatest",
-	"background":"img/delacroix/artist.jpg",
-	"fit":"vertical",
-	"down":{"title":"Liberty Leading the People"}
+var data = [
+[{
+	"title":"Delacroix, Eugene"
+	,"description":"Delacroix was the greatest"
+	,"background":"img/delacroix/artist.jpg"
+	,"fit":"vertical"
 }
 ,{
-	"title":"Picasso, Pablo",
-	"description": "He was pretty good also",
-	"background":"img/picasso/artist.jpg",
-	"fit":"vertical",
-	"down":{"title":"Guernica"
+	"title":"Liberty Leading the People"
+	,"description":"A great work"
+	,"background":"nah"
+	,"fit":"horizontal"
+}]
+,[{
+	"title":"Picasso, Pablo"
+	,"description": "He was pretty good also"
+	,"background":"img/picasso/artist.jpg"
+	,"fit":"vertical"
+}
+,{
+	"title":"Guernica"
 	,"description":"bombing of a town in WW2"
 	,"background":"img/picasso/guernica.jpg"
 	,"fit":"horizontal"
-	}
 }]
+]

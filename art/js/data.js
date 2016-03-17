ecc99ccf3af1ba894cbe5c817d2f3fee9a9d48b4
jwesly <wjabezkingsly@gmail.com>
2016-03-17 09:24:29 -0400

    nothing to see here

--- art/js/data.js
+++ art/js/data.js
@@ -8,9 +8,38 @@ var data = [
 }
 	,{
 		"title":"Liberty Leading the People"
-		,"description":"A great work"
-		,"background":"nah"
-		,"fit":"horizontal"
+		,"description":""
+		,"background":"img/delacroix/libertyLeadingThePeople.jpg"
+	}
+	,{
+		"title":"Death of Sardanapalus"
+		,"description":""
+		,"background":"img/delacroix/deathOfSardanapalus.jpg"
+	}
+	,{
+		"title":"Women of Algiers"
+		,"description":""
+		,"background":"img/delacroix/womenOfAlgiers.jpg"
+	}
+	,{
+		"title":"Greece on the Ruins of Missolonghi"
+		,"description":""
+		,"background":"img/delacroix/greece.jpg"
+	}
+	,{
+		"title":"Barque of the Dante"
+		,"description":""
+		,"background":"img/delacroix/barqueOfDante.jpg"
+	}
+	,{
+		"title":"Massacre at Chios"
+		,"description":""
+		,"background":"img/delacroix/massacreAtChios.jpg"
+	}
+	,{
+		"title":"Ovid Among the Scythians"
+		,"description":""
+		,"background":"img/delacroix/ovidAmongTheScythians.jpg"
 	}]
 ,[{
 	"title":"Picasso, Pablo"
@@ -44,47 +73,101 @@ var data = [
 		"title":"Las Meninas"
 		,"description":""
 		,"background":"img/velazquez/meninas.jpg"
-	}]
-,[{	"title":"Homer, Winslow"
-	,"description": ""
-	,"background":"img/homer/artist.jpg"
-	,"uri":"homer"
-}
+	}
 	,{
-		"title":"The Gulf Stream"
+		"title":"Rokeby Venus"
 		,"description":""
-		,"background":"img/homer/gulfstream.jpg"
-	}]
-,[{
-	"title":"West, Benjamin"
-	,"description": ""
-	,"background":"img/benjaminwest/artist.jpg"
-	,"uri":"west"
-}
+		,"background":"img/velazquez/rokebyVenus.jpg"
+	}
 	,{
-		"title":"Death of General Wolfe"
-		,"description":"A crouching Native American"
-		,"background":"img/benjaminwest/deathofwolf.jpg"
-	}]
-,[{	"title":"Sargent, John"
-	,"description": ""
-	,"background":"img/sargent/artist.jpg"
-	,"uri":"sargent"
-}
-	,{
-		"title":"Madame X"
+		"title":"Surrender of Breda"
 		,"description":""
-		,"background":"img/sargent/madamex.jpg"
+		,"background":"img/velazquez/surrenderOfBreda.jpg"
+	}
+	,{
+		"title":"Los Borrachos(Triumph of Bacchus)"
+		,"description":""
+		,"background":"img/velazquez/triumphOfBacchus.jpg"
+	}
+	,{
+		"title":"Portrait of Innocent X"
+		,"description":""
+		,"background":"img/velazquez/innocentX.jpg"
+	}
+	,{
+		"title":"Old Woman Frying Eggs"
+		,"description":""
+		,"background":"img/velazquez/oldWomanFryingEggs.jpg"
 	}]
-,[{	"title":"Whistler, James McNeill"
+,[{	"title":"El Greco"
 	,"description": ""
-	,"background":"img/whistler/artist.jpg"
-	,"uri":"whistler"
+	,"background":"img/elgreco/artist.jpg"
+	,"uri":""
 }
 	,{
-		"title":"Whistler's Mother"
+		"title":"View of Toledo"
 		,"description":""
-		,"background":"img/whistler/mother.jpg"
+		,"background":"img/elgreco/viewOfToledo.jpg"
+	}
+	,{
+		"title":"Burial of Count Orgaz"
+		,"description":""
+		,"background":"img/elgreco/burialOfCountOrgaz.jpg"
+	}
+	,{
+		"title":"Opening of the Fifth Seal"
+		,"description":""
+		,"background":"img/elgreco/openingTheFifthSeal.jpg"
+	}
+	,{
+		"title":"The Disrobing of Christ"
+		,"description":""
+		,"background":"img/elgreco/disrobingOfChrist.jpg"
+	}]
+,[{	"title":"de Goya, Francisco"
+	,"description": ""
+	,"background":"img/goya/artist.jpg"
+	,"uri":""
+}
+	,{
+		"title":"Third of May, 1808"
+		,"description":""
+		,"background":"img/goya/thirdOfMay.jpg"
+	}
+	,{
+		"title":"Saturn Devouring his Sons"
+		,"description":""
+		,"background":"img/goya/saturnDevouringHisSons.jpg"
+	}
+	,{
+		"title":"Sleep of Reason Produces Monsters"
+		,"description":""
+		,"background":"img/goya/sleepOfReasonProducesMonsters.jpg"
+	}
+	,{
+		"title":"Second of May, 1808: Charge of the Mamelukes"
+		,"description":""
+		,"background":"img/goya/chargeOfMamelukes.jpg"
+	}
+	,{
+		"title":"Duchess of Alba (Clothed Maja)"
+		,"description":""
+		,"background":"img/goya/clothedMaja.jpg"
+	}
+	,{
+		"title":"Nude Maja"
+		,"description":""
+		,"background":"img/goya/nudeMaja.jpg"
+	}]
+,[{	"title":""
+	,"description": ""
+	,"background":"img//artist.jpg"
+	,"uri":""
+}
+	,{
+		"title":""
+		,"description":""
+		,"background":"img//.jpg"
 	}]
 ,[{	"title":"Magritte, Rene"
 	,"description": "Magic Realism"
@@ -140,5 +223,277 @@ var data = [
 		"title":"False Mirror"
 		,"description":""
 		,"background":"img/magritte/falseMirror.jpg"
+	}]
+,[{	"title":"Rembrandt"
+	,"description": ""
+	,"background":"img/rembrandt/artist.jpg"
+	,"uri":"rembrandt"
+}
+	,{
+		"title":"Night Watch"
+		,"description":""
+		,"background":"img/rembrandt/nightWatch.jpg"
+	}
+	,{
+		"title":"the Anatomy Lesson of Dr. Tulp"
+		,"description":""
+		,"background":"img/rembrandt/anatomyLessonOfTulp.jpg"
+	}
+	,{
+		"title":"Aristotle Contemplating the Bust of Homer"
+		,"description":""
+		,"background":"img/rembrandt/aristotleContemplatingBustOfHomer.jpg"
+	}
+	,{
+		"title":"Belshazzar's Feast"
+		,"description":""
+		,"background":"img/rembrandt/belshazzarsFeast.jpg"
+	}
+	,{
+		"title":"The Polish Rider"
+		,"description":""
+		,"background":"img/rembrandt/polishRider.jpg"
+	}
+	,{
+		"title":"Abduction of Europa"
+		,"description":""
+		,"background":"img/rembrandt/abductionOfEuropa.jpg"
+	}]
+
+,[{	"title":"Vermeer"
+	,"description": "The background is known as the Procuress. Vermeer is believed to have portrayed himself at the upper right.<br>"
+	,"background":"img/vermeer/artist.jpg"
+	,"uri":""
+}
+	,{
+		"title":"Girl with Pearl Earring"
+		,"description":""
+		,"background":"img/vermeer/girlWithPearlEarring.jpg"
+	}
+	,{
+		"title":"The Geographer"
+		,"description":""
+		,"background":"img/vermeer/geographer.jpg"
+	}
+	,{
+		"title":"The Astronomer"
+		,"description":""
+		,"background":"img/vermeer/astronomer.jpg"
+	}
+	,{
+		"title":"View of Delft"
+		,"description":""
+		,"background":"img/vermeer/viewOfDelft.jpg"
+	}
+	,{
+		"title":"The Art of Painting"
+		,"description":""
+		,"background":"img/vermeer/artOfPainting.jpg"
+	}
+	,{
+		"title":"The Love Letter"
+		,"description":""
+		,"background":"img/vermeer/loveLetter.jpg"
+	}
+	,{
+		"title":"The Music Lesson"
+		,"description":""
+		,"background":"img/vermeer/musicLesson.jpg"
+	}]
+,[{	"title":"van Gogh, Vincent"
+	,"description": ""
+	,"background":"img/vangogh/artist.jpg"
+	,"uri":"vangogh"
+}
+	,{
+		"title":"Starry Night"
+		,"description":""
+		,"background":"img/vangogh/starryNight.jpg"
+	}
+	,{
+		"title":"The Night Cafe"
+		,"description":""
+		,"background":"img/vangogh/nightCafe.jpg"
+	}
+	,{
+		"title":"Potato Eaters"
+		,"description":""
+		,"background":"img/vangogh/potatoEaters.jpg"
+	}
+	,{
+		"title":"Bedroom at Arles"
+		,"description":""
+		,"background":"img/vangogh/bedroomAtArles.jpg"
+	}
+	,{
+		"title":"Portrait of Dr. Gachet(1st)"
+		,"description":""
+		,"background":"img/vangogh/portraitOfGachet.jpg"
+	}
+	,{
+		"title":"Crows in Wheatfield"
+		,"description":""
+		,"background":"img/vangogh/crowsInWheatfield.jpg"
+	}]
+,[{	"title":"Jan Van Eyck"
+	,"description": "Background aka Man in Red Turban. It's most likely a self-portrait<br><ul><li>Court painter for Philip the Good</li><li>had a brother Hubert</li><li>Ghent Altarpiece is his masterpiece</li></ul>"
+	,"background":"img/vaneyck/artist.jpg"
+	,"uri":"vaneyck"
+}
+	,{
+		"title":"Arnolfini Wedding"
+		,"description":""
+		,"background":"img/vaneyck/arnolfiniWedding.jpg"
+	}
+	,{
+		"title":"Adoration of the Lamb"
+		,"description":"Part of the Ghent Altarpiece"
+		,"background":"img/vaneyck/adorationOfTheLamb.jpg"
+	}
+	,{
+		"title":"Madonna of Chancellor Rolin"
+		,"description":"<ul><li>peacocks in background</li><li>Floating angel crowns Virgin Mary</ul>"
+		,"background":"img/vaneyck/madonnaOfChancellorRolin.jpg"
+	}
+	,{
+		"title":"Madonna of the Canon van der Paele"
+		,"description":""
+		,"background":"img/vaneyck/madonnaOfCanonPaele.jpg"
+	}]
+,[{	"title":"David, Jacques Louis"
+	,"description": ""
+	,"background":"img/david/artist.jpg"
+	,"uri":"david"
+}
+	,{
+		"title":"The Oath of the Horatii"
+		,"description":""
+		,"background":"img/david/oathOfHoratii.jpg"
+	}
+	,{
+		"title":"Death of Socrates"
+		,"description":""
+		,"background":"img/david/deathOfSocrates.jpg"
+	}
+	,{
+		"title":"Death of Marat"
+		,"description":""
+		,"background":"img/david/deathOfMarat.jpg"
+	}
+	,{
+		"title":"Napoleon Crossing the Alps"
+		,"description":""
+		,"background":"img/david/napoleonCrossingAlps.jpg"
+	}
+	,{
+		"title":"Portrait of Madame Recamier"
+		,"description":""
+		,"background":"img/david/madameRecamier.jpg"
+	}
+	,{
+		"title":"Tennis Court Oath"
+		,"description":""
+		,"background":"img/david/tennisCourtOath.jpg"
+	}
+	,{
+		"title":"Intervention of the Sabine Women"
+		,"description":""
+		,"background":"img/david/interventionOfSabineWomen.jpg"
+	}
+	,{
+		"title":"The Lictors Bring Brutus the Bodies of His Sons"
+		,"description":""
+		,"background":"img/david/lictorsBringBrutusBodiesOfHisSons.jpg"
+	}
+	,{
+		"title":"Coronation of Napoleon and Josephine"
+		,"description":""
+		,"background":"img/david/coronationOfNapoleon.jpg"
+	}]
+,[{	"title":"Sandro Boticelli"
+	,"description": ""
+	,"background":"img/boticelli/artist.jpg"
+	,"uri":"boticelli"
+}
+	,{
+		"title":"Birth of Venus"
+		,"description":""
+		,"background":"img/boticelli/birthOfVenus.jpg"
+	}
+	,{
+		"title":"Primavera"
+		,"description":""
+		,"background":"img/boticelli/primavera.jpg"
+	}
+	,{
+		"title":"Mystic Nativity"
+		,"description":""
+		,"background":"img/boticelli/mysticNativity.jpg"
+	}
+	,{
+		"title":"Madonna of the Magnificat"
+		,"description":""
+		,"background":"img/boticelli/madonnaMagnificat.jpg"
+	}]
+,[{	"title":""
+	,"description": ""
+	,"background":"img//artist.jpg"
+	,"uri":""
+}
+	,{
+		"title":""
+		,"description":""
+		,"background":"img//.jpg"
+	}]
+,[{	"title":""
+	,"description": ""
+	,"background":"img//artist.jpg"
+	,"uri":""
+}
+	,{
+		"title":""
+		,"description":""
+		,"background":"img//.jpg"
+	}]
+,[{	"title":"Homer, Winslow"
+	,"description": ""
+	,"background":"img/homer/artist.jpg"
+	,"uri":"homer"
+}
+	,{
+		"title":"The Gulf Stream"
+		,"description":""
+		,"background":"img/homer/gulfstream.jpg"
+	}]
+,[{
+	"title":"West, Benjamin"
+	,"description": ""
+	,"background":"img/benjaminwest/artist.jpg"
+	,"uri":"west"
+}
+	,{
+		"title":"Death of General Wolfe"
+		,"description":"A crouching Native American"
+		,"background":"img/benjaminwest/deathofwolf.jpg"
+	}]
+,[{	"title":"Sargent, John"
+	,"description": ""
+	,"background":"img/sargent/artist.jpg"
+	,"uri":"sargent"
+}
+	,{
+		"title":"Madame X"
+		,"description":""
+		,"background":"img/sargent/madamex.jpg"
+	}]
+,[{	"title":"Whistler, James McNeill"
+	,"description": ""
+	,"background":"img/whistler/artist.jpg"
+	,"uri":"whistler"
+}
+	,{
+		"title":"Whistler's Mother"
+		,"description":""
+		,"background":"img/whistler/mother.jpg"
 	}]
 ]
